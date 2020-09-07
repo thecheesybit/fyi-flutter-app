b6958c550efb14a052b0ffca79034b835821c2c7
Ayush Kumar <55230372+akbeingstupid@users.noreply.github.com>
2020-09-08 01:37:36 +0530

    second change

--- lib/screens/home/homePage.dart
+++ lib/screens/home/homePage.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:hello_dummy/auth/authPackage.dart';
+
 
 class HomePage extends StatefulWidget {
+ 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+ final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('hello'),
-    ),
+          title: Text('HOME PAGE', style: TextStyle(color: Colors.black),),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.lock),
+              label: Text('log out'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+            // FlatButton.icon(
+            //   icon: Icon(Icons.settings),
+            //   label: Text('update'),
+            //   onPressed: () => _showSettingsPanel(),
+            // )
+          ],
+        ),
     );
   }
 }
