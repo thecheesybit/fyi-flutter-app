338e7e2bc868fdedeb9f64bcf46ca16b985f62a0
Ayush Kumar <55230372+akbeingstupid@users.noreply.github.com>
2020-09-09 20:28:34 +0530

    loading changed

--- lib/shared/laoding.dart
+++ lib/shared/laoding.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
+const int _color1 = 0xff7874FD;
+const int _color2 = 0xffB225EE;
+
 class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       constraints: BoxConstraints.expand(),
-              decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [Color(0xff7874FD), Color(0xffB225EE)]),
-                    ),
-                // image: DecorationImage(
-                //     image: AssetImage("assets/images/b3.gif"),
-                //     fit: BoxFit.cover),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(_color1), Color(_color2)]),
+      ),
+      // image: DecorationImage(
+      //     image: AssetImage("assets/images/b3.gif"),
+      //     fit: BoxFit.cover),
       child: Center(
         child: SpinKitWave(
           color: Colors.pink[400],
